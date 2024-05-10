9caccf36013c295bc3b8449412ceca8e71b21df3
GaelGuevara <166858942+GaelGuevara@users.noreply.github.com>
2024-05-10 16:14:41 -0300

    Update M_Pap.h

--- M_Pap.h
+++ M_Pap.h
@@ -1,8 +1,9 @@
 #ifndef M_PAP_H
 #define M_PAP_H
 
-void pasoC(int Pasos);
-void medP();
-void 
+void pasoC(int pasos);
+void pasoN(int pasos);
+void pasoM(int pasos);
+void pasoNMC(char mode, int pasos);
 
 #endif
